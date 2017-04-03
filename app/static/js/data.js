adda91c7e58309cf54120f23cddcc9508210d8c1
Yikai <ywang8@stuy.edu>
2017-04-03 14:49:29 -0400

    made some more progress

--- app/static/js/data.js
+++ app/static/js/data.js
@@ -26,4 +26,29 @@ databg.setAttribute("height", datah);
 databg.setAttribute("style", "fill:rgb(239, 239, 255)");
 data.appendChild(databg);
 
+var rid = 0;
+var xcor, ycor;
+
+var stop = function(){
+    window.cancelAnimationFrame(rid);
+};
+
+var clear = function(){
+    while (svg.lastChild){
+        svg.removeChild(svg.lastChild);
+    }
+};
+
+/* keeps track of the xcor and ycor of mouse */
+svg.addEventListener("mousemove", function(evt) {
+    xcor = evt.offsetX;
+    ycor = evt.offsetY;
+});
+
+var data_years = function() {
+    svg.selectAll("div")
+        .data(info)
+        .enter()
+        .append("div")
+};
 console.log("Loaded js.")
